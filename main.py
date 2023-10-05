95e4a95d436edb1bac629b05e0b66b8f2e0ee024
Khaopiek <83733282+Khaopiek@users.noreply.github.com>
2023-10-05 23:52:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ from credentials import TELEGRAM_API_KEY, BINANCE_API_KEY, BINANCE_API_SECRET, S
 binance_client = Client(BINANCE_API_KEY, BINANCE_API_SECRET)
 telegram_bot = telegram.Bot(token=TELEGRAM_API_KEY)
 
+def get_trimmed_quantity(quantity, step_size):
+    trimmed_quantity = round(quantity, step_size)
+    return trimmed_quantity
+
+def get_trimmed_price(price, tick_size):
+    trimmed_price = round(price, tick_size)
+    return trimmed_price
+
+# Fetch precision for BTCUSDT (or the asset you're trading)
+info = binance_client.futures_exchange_info()
+precision_data = next((item for item in info['symbols'] if item["symbol"] == "BTCUSDT"), None)
+quantity_precision = precision_data['quantityPrecision']
+price_precision = precision_data['pricePrecision']
+
 # Constants
 ORDER_PERCENTAGE = 10  # Placeholder value, set to desired percentage
 
@@ -40,8 +54,8 @@ from telegram_message_sender import send_message
 # Your original global variables
 # Define the moving averages
 MA1 = [1]
-MA2 = [20]
-MA3 = [100]
+MA2 = [50]
+MA3 = [200]
 
 # Define the indicators
 I1 = 0
@@ -79,10 +93,14 @@ def on_message(ws, message):
     # Buy logic
     if I1 + I2 + I3 == 2:
         # Fetch the available balance (assuming using USDT for simplicity)
-        balance = get_balance("TUSD")
+        balance = get_balance("USDT")
         order_amount = calculate_order_amount(balance, ORDER_PERCENTAGE)  # Using the predefined ORDER_PERCENTAGE
         # Place buy order (using a placeholder for symbol and order type for simplicity)
-        order = place_order("BTCTUSD", "buy", order_amount)
+        
+# Truncate the order amount using the fetched precision before placing an order
+order_amount = get_trimmed_quantity(order_amount, quantity_precision)
+order = place_order("BTCUSDT", "buy", order_amount)
+
         
         # Update the last buy transaction details
         last_buy_price = order['fills'][0]['price']
@@ -90,14 +108,21 @@ def on_message(ws, message):
         
         send_message(f"Buy Signal!\nBought at price: {last_buy_price}\nAmount: {last_buy_amount}")
     
-    # Sell logic
-    elif I1 + I2 + I3 >= 5:
-        # Calculate net profit
-        net_profit = (close_price - last_buy_price) * last_buy_amount
-        
-        send_message(f"Sell Signal!\nBought at price: {last_buy_price}\nSold at price: {close_price}\nAmount: {last_buy_amount}\nNet Profit: {net_profit}")
-        
-        # Reset the last buy transaction details
+    
+# Check if last_buy_price is not 0 before executing sell logic
+if last_buy_price == 0:
+    print("No previous buy order to reference for selling.")
+    return
+
+
+    # Calculate net profit
+    net_profit = (close_price - last_buy_price) * last_buy_amount
+
+    # Place market sell order (using a placeholder for symbol and order type for simplicity)
+    order = place_order("BTCUSDT", "sell", last_buy_amount)
+
+    send_message(f"Sell Signal!\nBought at price: {last_buy_price}\nSold at price: {close_price}\nAmount: {last_buy_amount}\nNet Profit: {net_profit}")
+# Reset the last buy transaction details
         last_buy_price = 0
         last_buy_amount = 0
 
@@ -106,7 +131,8 @@ def on_error(ws, error):
     print(f"Error: {error}")
 
 def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
+        telegram_bot.sendMessage(chat_id=TELEGRAM_USER_ID_LIST[0], text="Bot has stopped")
+print("### closed ###")
 
 def on_open(ws):
     # Subscribe to BTC TUSD 1s interval candlestick data
@@ -115,6 +141,8 @@ def on_open(ws):
         "params": ["btcusdt@kline_1s"],
         "id": 1
     }))
+
+telegram_bot.sendMessage(chat_id=TELEGRAM_USER_ID_LIST[0], text="Bot has started")
 
 ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/btcusdt@kline_1s",
                             on_message=on_message,
